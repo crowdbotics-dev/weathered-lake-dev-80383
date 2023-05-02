b2beffcb6ed48ce5cd7c9faf4be3c07109afe1ed
Crowdbotics <engineering@crowdbotics.com>
2023-05-02 13:56:12 +0000

    Initial commit.

---
+++ backend/weathered_lake_dev_80383/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for weathered_lake_dev_80383 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weathered_lake_dev_80383.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-lake-dev-80383",
+  url: "https://weathered-lake-dev-80383.botics.co",
+  api: "https://weathered-lake-dev-80383.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
